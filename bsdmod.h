8702f6bf7870ca136329aa1c5aad4e63e33e9cf3
Lloyd Dilley <ldilley@users.noreply.github.com>
2018-12-31 01:09:32 -0500

    Add module dependency example

---
+++ bsdmod.h
@@ -0,0 +1,6 @@
+#ifndef BSDMOD_H
+#define BSDMOD_H
+
+extern void bsdmod_sum(int x, int y);
+
+#endif /* BSDMOD_H */
--- bsdmod2.c
+++ bsdmod2.c
@@ -1,7 +1,7 @@
-/* bsdmod.c */
+/* bsdmod2.c */
 
 /*
- * An example FreeBSD kernel module
+ * An example FreeBSD kernel module to demonstrate module dependency
  */
 
 /* The order of these headers is significant */
@@ -14,17 +14,12 @@
 #include <sys/kernel.h>
 #include "bsdmod.h"
 
-#define MODULE_NAME "bsdmod"
+#define MODULE_NAME "bsdmod2"
 
-MODULE_VERSION(bsdmod, 1);
+MODULE_VERSION(bsdmod2, 1);
+MODULE_DEPEND(bsdmod2, bsdmod, 1, 1, 5);
 
-void bsdmod_sum(int x, int y)
-{
-  printf("%s->%s(%d, %d): %d + %d = %d\n", MODULE_NAME, __func__, x, y, x, y, x + y);
-  return;
-}
-
-static int bsdmod_handler(struct module *m, int what, void *arg)
+static int bsdmod2_handler(struct module *m, int what, void *arg)
 {
   int err = 0;
 
@@ -32,6 +27,7 @@ static int bsdmod_handler(struct module *m, int what, void *arg)
   {
     case MOD_LOAD:
       printf("%s->%s(): Kernel module loaded.\n", MODULE_NAME, __func__);
+      bsdmod_sum(2, 3);
       break;
     case MOD_UNLOAD:
       printf("%s->%s(): Kernel module unloaded.\n", MODULE_NAME, __func__);
@@ -43,11 +39,11 @@ static int bsdmod_handler(struct module *m, int what, void *arg)
   return(err);
 }
 
-static moduledata_t bsdmod_mod =
+static moduledata_t bsdmod2_mod =
 {
   MODULE_NAME,
-  bsdmod_handler,
+  bsdmod2_handler,
   NULL
 };
 
-DECLARE_MODULE(bsdmod, bsdmod_mod, SI_SUB_KLD, SI_ORDER_ANY);
+DECLARE_MODULE(bsdmod2, bsdmod2_mod, SI_SUB_KLD, SI_ORDER_ANY);
